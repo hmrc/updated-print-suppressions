4874597ca38f0f41d9e2ac2001fb1edc3d7cfe92
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2023-11-17 13:01:15 +0000

    DC-5722 Update dependencies (#33)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -20,36 +20,36 @@ import sbt._
 
 object AppDependencies {
 
-  val AkkaVersion      = "2.6.20"
-  val hmrcMongo        = "1.2.0"
-  val bootstrapBackend = "7.15.0"
-    
+  val AkkaVersion = "2.6.21"
+  val hmrcMongo = "1.4.0"
+  val bootstrapBackend = "7.23.0"
+
   val compile = Seq(
     ws,
-    "com.typesafe.akka"         %% "akka-actor"                        % AkkaVersion,
-    "com.typesafe.akka"         %% "akka-actor-typed"                  % AkkaVersion,
-    "com.typesafe.akka"         %% "akka-protobuf-v3"                  % AkkaVersion,
-    "com.typesafe.akka"         %% "akka-stream"                       % AkkaVersion,
-    "com.typesafe.akka"         %% "akka-serialization-jackson"        % AkkaVersion,
-    "uk.gov.hmrc"               %% "bootstrap-backend-play-28"         % bootstrapBackend,
-    "uk.gov.hmrc.mongo"         %% "hmrc-mongo-work-item-repo-play-28" % hmrcMongo,
-    "uk.gov.hmrc"               %% "domain"                            % "8.3.0-play-28",
-    "net.codingwell"            %% "scala-guice"                       % "5.1.1",
-    "com.typesafe.play"         %% "play-json-joda"                    % "2.9.4"
+    "com.typesafe.akka" %% "akka-actor"                        % AkkaVersion,
+    "com.typesafe.akka" %% "akka-actor-typed"                  % AkkaVersion,
+    "com.typesafe.akka" %% "akka-protobuf-v3"                  % AkkaVersion,
+    "com.typesafe.akka" %% "akka-stream"                       % AkkaVersion,
+    "com.typesafe.akka" %% "akka-serialization-jackson"        % AkkaVersion,
+    "uk.gov.hmrc"       %% "bootstrap-backend-play-28"         % bootstrapBackend,
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-28" % hmrcMongo,
+    "uk.gov.hmrc"       %% "domain"                            % "8.3.0-play-28",
+    "net.codingwell"    %% "scala-guice"                       % "5.1.1",
+    "com.typesafe.play" %% "play-json-joda"                    % "2.9.4"
   )
 
   val test = Set(
-    "uk.gov.hmrc"                  %% "bootstrap-test-play-28"    % bootstrapBackend    % "test, it",
-    "uk.gov.hmrc.mongo"            %% "hmrc-mongo-test-play-28"   % hmrcMongo           % "test,it",
-    "com.github.tomakehurst"       %  "wiremock-standalone"       % "2.27.2"            % "test,it",
-    "org.mockito"                  %  "mockito-core"              % "5.2.0"             % "test,it",
-    "org.scalatestplus"            %% "mockito-3-4"               % "3.2.10.0"          % "test,it",
-    "org.scalatest"                %% "scalatest"                 % "3.2.15"            % "test,it",
-    "org.pegdown"                  %  "pegdown"                   % "1.6.0"             % "test,it",
-    "org.scalatestplus.play"       %% "scalatestplus-play"        % "5.1.0"             % "test,it",
-    "com.typesafe.play"            %% "play-test"                 % PlayVersion.current % "test,it",
-    "com.vladsch.flexmark"         %  "flexmark-all"              % "0.62.2"            % "test,it",
-    "com.typesafe.akka"            %% "akka-testkit"              % AkkaVersion         % Test
+    "uk.gov.hmrc"            %% "bootstrap-test-play-28"  % bootstrapBackend    % "test, it",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongo           % "test,it",
+    "com.github.tomakehurst" % "wiremock-standalone"      % "2.27.2"            % "test,it",
+    "org.mockito"            % "mockito-core"             % "5.2.0"             % "test,it",
+    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"          % "test,it",
+    "org.scalatest"          %% "scalatest"               % "3.2.15"            % "test,it",
+    "org.pegdown"            % "pegdown"                  % "1.6.0"             % "test,it",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"             % "test,it",
+    "com.typesafe.play"      %% "play-test"               % PlayVersion.current % "test,it",
+    "com.vladsch.flexmark"   % "flexmark-all"             % "0.62.2"            % "test,it",
+    "com.typesafe.akka"      %% "akka-testkit"            % AkkaVersion         % Test
   )
 
   val overrides = Set()
