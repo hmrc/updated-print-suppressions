a870d28a3758e996a0ac33f8ec2621f411f4bc65
Kapil Sanghvi <22366606+kjainsc@users.noreply.github.com>
2024-03-28 15:30:31 +0000

    DC-6202 Remove play-json-joda dependency (#41)
    * DC-6202 Remove play-json-joda dependency
    
    * DC-6202 Updates as per pr bot comments

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -21,8 +21,8 @@ import sbt._
 object AppDependencies {
 
   val PekkoVersion = "1.0"
-  val hmrcMongo = "1.4.0"
-  val bootstrapBackend = "8.4.0"
+  val hmrcMongo = "1.8.0"
+  val bootstrapBackend = "8.5.0"
 
   val compile = Seq(
     ws,
@@ -30,23 +30,19 @@ object AppDependencies {
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-work-item-repo-play-30" % hmrcMongo,
     "uk.gov.hmrc"       %% "domain-play-30"                    % "9.0.0",
     "net.codingwell"    %% "scala-guice"                       % "5.1.1",
-    "com.typesafe.play" %% "play-json-joda"                    % "2.9.4",
     "org.typelevel"     %% "cats-core"                         % "2.9.0"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapBackend    % "test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongo           % "test",
-    "org.mockito"            %  "mockito-core"            % "5.2.0"             % "test",
-    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"          % "test",
-    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.0"             % "test"
+    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapBackend % "test",
+    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongo        % "test",
+    "org.mockito"            % "mockito-core"             % "5.2.0"          % "test",
+    "org.scalatestplus"      %% "mockito-3-4"             % "3.2.10.0"       % "test",
+    "org.scalatestplus.play" %% "scalatestplus-play"      % "7.0.0"          % "test"
   )
 
   val it = Seq(
-    "uk.gov.hmrc"            %% "bootstrap-test-play-30"  % bootstrapBackend    % "it/test",
-    "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-30" % hmrcMongo           % "it/test"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapBackend % "it/test",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongo        % "it/test"
   )
-  
-  val overrides = Set()
-
 }
