0a1828a46b97f6bdae0713a2dbb0c2ad5642b140
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-10-30 13:03:03 +0000

    DC-7963: clean unused import

--- it/test/uk/gov/hmrc/ups/controllers/UpdatedPrintSuppressionsControllerISpec.scala
+++ it/test/uk/gov/hmrc/ups/controllers/UpdatedPrintSuppressionsControllerISpec.scala
@@ -16,7 +16,6 @@
 
 package uk.gov.hmrc.ups.controllers
 
-import org.scalatest.Ignore
 import org.scalatest.concurrent.IntegrationPatience
 import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.{ JsArray, JsValue, Json }
