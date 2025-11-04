77bf7cf3ea2b1ca278f1e74412fd0062e88c4862
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-11-04 09:29:34 +0000

    DC-7963: Added tests for DateConverterSpec
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage
    
    DC-7963: Add unit tests to improve test coverage

---
+++ test/uk/gov/hmrc/ups/model/PulledItemSpec.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ups.model
+
+import org.scalatest.funsuite.AnyFunSuiteLike
+import org.scalatestplus.play.PlaySpec
+import play.api.libs.json.Json
+import uk.gov.hmrc.ups.utils.DateFormats.instantFormats
+import java.util.UUID
+
+class PulledItemSpec extends PlaySpec {
+  "PulledItem" should {
+    "serialize and deserialize" in {
+      implicit val pulledItemFormat = PulledItem.formats
+      val originalPulledItem = PulledItem(
+        entityId = EntityId("some-id"),
+        paperless = true,
+        updatedAt = instantFormats.reads(Json.parse("\"2024-06-10T12:34:56Z\"")).get,
+        callbackUrl = s"https://callback.url/${UUID.randomUUID()}"
+      )
+      val json = Json.toJson(originalPulledItem)
+      val deserializedPulledItem = json.as[PulledItem]
+      deserializedPulledItem mustEqual originalPulledItem
+    }
+  }
+}
