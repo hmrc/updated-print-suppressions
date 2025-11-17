d14dbfc8233cb5a13d8cc1e464850a444b140f8a
Murali raju <17339891+muralishub@users.noreply.github.com>
2025-11-17 15:15:18 +0000

    DC-8059: Revert temporary logs (#59)

--- app/uk/gov/hmrc/ups/repository/UpdatedPrintSuppressionsDatabase.scala
+++ app/uk/gov/hmrc/ups/repository/UpdatedPrintSuppressionsDatabase.scala
@@ -18,26 +18,18 @@ package uk.gov.hmrc.ups.repository
 
 import uk.gov.hmrc.mongo.MongoComponent
 import org.mongodb.scala.ObservableFuture
-import play.api.Logger
-
-import scala.concurrent.duration._
 import javax.inject.{ Inject, Singleton }
-import scala.concurrent.{ Await, ExecutionContext, Future }
+import scala.concurrent.{ ExecutionContext, Future }
 
 @Singleton
 class UpdatedPrintSuppressionsDatabase @Inject() (mongoComponent: MongoComponent) {
-  val logger: Logger = Logger(this.getClass)
-  def dropCollection(collectionName: String)(implicit ec: ExecutionContext): Future[Unit] = {
-    logger.warn("dropCollection invoked - this will drop a ups collection")
+
+  def dropCollection(collectionName: String)(implicit ec: ExecutionContext): Future[Unit] =
     mongoComponent.database.getCollection(collectionName).drop().toFuture().map(_ => ())
-  }
 
   private def listCollectionNames(predicate: String => Boolean)(implicit ec: ExecutionContext): Future[List[String]] =
     mongoComponent.database.listCollectionNames().toFuture().map(_.filter(predicate).toList)
 
-  def upsCollectionNames(implicit ec: ExecutionContext): Future[List[String]] = {
-    val count = mongoComponent.database.listCollectionNames().toFuture().map(_.size)
-    logger.warn(s"upsCollectionNames invoked - count is ${Await.result(count, 10.seconds)}")
+  def upsCollectionNames(implicit ec: ExecutionContext): Future[List[String]] =
     listCollectionNames(_.startsWith("updated"))
-  }
 }
